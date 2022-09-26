61a0715da4f54235d8bfb7f3f8635f54a379b7f3
DRG2Boss <113391397+DRG2Boss@users.noreply.github.com>
2022-09-26 17:34:36 -0400

    Presentation of output data polished

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ for lines in sdata_lines:
     sdata_seperated = lines.split(':')
     sid = sdata_seperated[0]
     sgpa = sdata_seperated[2]
-    print(f"{sid}:{sgpa}")
+    print(f"ID #{sid}, {sgpa}")
